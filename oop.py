fcc1fa2a04a1292ea0779aadaa04cb5db1f48ed7
lalebdi <loai.alebdi@gmail.com>
2021-07-12 15:22:49 -0400

    modifing

--- oop.py
+++ oop.py
@@ -1,15 +1,23 @@
 class Instructors:
     companyName = "Upper West Side"
 
-    def __init__(self, course):
+    def __init__(self, course, duration):
         self.course = course
+        self.duration = duration
 
     def printInfo(self):
         print("My company name is", self.companyName)
 
 
-elearning = Instructors("Python")
-inPerson = Instructors("Java")
+elearning = Instructors("Python", 3)
+inPerson = Instructors("Java", 2)
 
 elearning.printInfo()
 print(elearning.course)
+print(inPerson.duration)
+
+inPerson.course = "Django"
+
+print(inPerson.course)
+
+# DEL elearning.duration
