03069c5d9c7e437c6a164aaddff4fe5150678bb0
lalebdi <loai.alebdi@gmail.com>
2021-07-12 15:53:18 -0400

    sub constructor

--- inheritance.py
+++ inheritance.py
@@ -11,10 +11,23 @@ florist = Person("Jane", "Doe")
 florist.printName()
 
 
-class Lawyers(Person): # if you create a child class without a constructor, it will inherit all methods and attributes
+class OldLawyers(Person): # if you create a child class without a constructor, it will inherit all methods and attributes
     pass
 
 
-immigration_lawyer = Lawyers("John", "Blah")
+immigration_lawyer = OldLawyers("John", "Blah")
 
 immigration_lawyer.printName()
+
+
+class Lawyers(Person):
+    def __init__(self, firstname, lastname):
+        Person.__init__(self, firstname, lastname)
+        # self.firstName = firstname
+        # self.lastName = lastname
+
+    def printInfo(self):
+        print(self.firstName, self.lastName)
+
+civil_lawyer = Lawyers("Simely", "Teeth")
+civil_lawyer.printInfo()
