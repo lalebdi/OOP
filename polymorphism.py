353bfc8686482e1e18c32b9bb13d0ec503a435ec
lalebdi <loai.alebdi@gmail.com>
2021-07-12 16:33:32 -0400

    polymorphic classes

--- polymorphism.py
+++ polymorphism.py
@@ -1,4 +1,24 @@
-def addNumbers(a, b, c= 1):
+def addNumbers(a, b, c=1):
     return a + b + c
 
+
 print(addNumbers(8, 9))
+
+
+print(addNumbers(8, 9, 3))
+
+
+class UK():
+    def capital_city(self):
+        print("London")
+
+    def language(self):
+        print("English")
+
+
+class Spain():
+    def capital_city(self):
+        print("Madrid")
+
+    def language(self):
+        print("Spanish")
