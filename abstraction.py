23fc63be04fc10e9e8ed8efd0d9fd76d7939a742
lalebdi <loai.alebdi@gmail.com>
2021-07-12 20:56:04 -0400

    example

--- abstraction.py
+++ abstraction.py
@@ -1,13 +1,30 @@
-class Shape:
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
     def area(self):
         pass
 
+    @abstractmethod
     def perimeter(self):
         pass
 
+
 class Square(Shape):
     def __init__(self, side):
-        self.side = side
+        self.__side = side
+
+    def area(self):
+        return self.__side * self.__side
+
+    def perimeter(self):
+        return self.__side * 4
 
 
-myShape = Shape()
+# myShape = Shape()
+
+mysquare = Square(5)
+
+print(mysquare.area())
+
