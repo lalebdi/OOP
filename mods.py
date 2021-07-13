a055ab5d200001fe13f2e0e626d76b8c7eff1600
lalebdi <loai.alebdi@gmail.com>
2021-07-13 15:36:07 -0400

    sys

--- mods.py
+++ mods.py
@@ -1,6 +1,13 @@
 # OS module to change directory
-import os
+import os, sys
 os.mkdir("tempdir")
 os.chdir("tempdir")
 
 os.getcwd() # shows the directory
+
+# os.listdir # sub directories
+
+
+# sys module: functions and variables used to manipulate python's runtime env.
+
+
