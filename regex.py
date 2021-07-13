c3f2bcafb9362f50155f5cdf4c729bd2bc625c94
lalebdi <loai.alebdi@gmail.com>
2021-07-13 14:48:39 -0400

    findall

--- regex.py
+++ regex.py
@@ -1,9 +1,15 @@
 import re
 
 newTxt = "You can start learning Python"
-x = re.search("^You.*Python", newTxt)
+x = re.search("^You.*Python$", newTxt) # starts with You and ends with Python
 
 if x:
     print("Yes")
 else:
     print("No")
+
+
+anotherTxt = "Python is very very very easy"
+y = re.findall("very", anotherTxt)
+print(y)
+
