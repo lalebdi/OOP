9169d0bddbfe0eb90d38d202ee462cca807bf546
lalebdi <loai.alebdi@gmail.com>
2021-07-13 15:13:37 -0400

    search

--- regex.py
+++ regex.py
@@ -13,3 +13,20 @@ anotherTxt = "Python is very very very easy"
 y = re.findall("very", anotherTxt)
 print(y)
 
+otherTxt = "Python is very easy"
+z = re.search("\s", otherTxt)
+print("The first white space index= ", z.start())
+
+a = re.sub("\s", "$", otherTxt)
+print(a)
+
+txt = "Python is very very easy"
+b = re.search("very", txt)
+print(b.span())
+
+# Searching for upper case C in the beginning of a word and prints its position
+something = "Code with Python now"
+c = re.search(r"\bC\w+", something)
+print(c.span())
+print(c.string)
+print(c.group())
